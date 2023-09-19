db4f1c67a34b4f8627b96f5202c151461a7887c0
Reza Heidari <m.heidari988@gmail.com>
2023-09-19 21:06:10 +0300

    add menu component

---
+++ src/components/home/Menu.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Menu = (props) => {
+  function openForm() {
+    document.getElementById("contact-card").style.display = "block";
+  }
+
+  return (
+    <ul className="menu">
+      {props.menu.map((m) => {
+        if (m.url === "#cv") {
+          return (
+            <li key={m.title}>
+              <a
+                id={m.title.toLowerCase()}
+                className="underline_from_left"
+                href="/Resume.pdf"
+                download
+              >
+                {m.title.toUpperCase()}
+              </a>
+            </li>
+          );
+        } else {
+          return (
+            <li key={m.title}>
+              <a
+                id={m.title.toLowerCase()}
+                className="underline_from_left"
+                href={m.url}
+                onClick={m.url === "#openForm" ? openForm : undefined}
+              >
+                {m.title.toUpperCase()}
+              </a>
+            </li>
+          );
+        }
+      })}
+    </ul>
+  );
+};
+
+export default Menu;
