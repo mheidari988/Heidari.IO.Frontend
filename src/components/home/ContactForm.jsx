6c8741b3fc220bfde3f5dce104f051f9a2ecf4eb
Reza Heidari <m.heidari988@gmail.com>
2023-09-20 00:41:54 +0300

    fix for react warning of for => htmlFor

--- src/components/home/ContactForm.jsx
+++ src/components/home/ContactForm.jsx
@@ -8,13 +8,13 @@ const ContactForm = () => {
   return (
     <div id="contact-card">
       <form id="contact-form" action="" method="post">
-        <label for="fullName">Full name</label>
+        <label htmlFor="fullName">Full name</label>
         <input type="text" name="fullName" id="fullName" />
-        <label for="email">Email</label>
+        <label htmlFor="email">Email</label>
         <input type="text" name="email" id="email" />
-        <label for="subject">Subject</label>
+        <label htmlFor="subject">Subject</label>
         <input type="text" name="subject" id="subject" />
-        <label for="message">Message</label>
+        <label htmlFor="message">Message</label>
         <textarea id="message" rows="4" cols="50"></textarea>
         <input id="submit-msg" type="button" value="Send" />
       </form>
