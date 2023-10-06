88527a658838e50e9ba73075c0197add651d47c1
Reza Heidari <m.heidari988@gmail.com>
2023-10-06 20:21:00 +0300

    Update Footer.jsx
    to fetch data from API instead of hard coded information

--- src/components/home/Footer.jsx
+++ src/components/home/Footer.jsx
@@ -20,30 +20,7 @@ const Footer = () => {
 
   return (
     <footer>
-      This was coded in{" "}
-      <a href="https://code.visualstudio.com/" target="_blank">
-        Visual Studio Code
-      </a>{" "}
-      by yours truly. It was built with{" "}
-      <a href="https://react.dev/" target="_blank">
-        React
-      </a>
-      , styled with{" "}
-      <a
-        href="https://developer.mozilla.org/en-US/docs/Web/CSS"
-        target="_blank"
-      >
-        CSS
-      </a>
-      , managed by{" "}
-      <a href="https://dotnet.microsoft.com/en-us/" target="_blank">
-        .NET Core backend
-      </a>
-      , Containerized by{" "}
-      <a href="https://www.docker.com/" target="_blank">
-        Docker
-      </a>
-      , and published on my <a href="https://www.linux.org/">Linux</a> server.
+      <span dangerouslySetInnerHTML={{ __html: version.description }}></span>
       [Version {version.informationalVersion}]
     </footer>
   );
