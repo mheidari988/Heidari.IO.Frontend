c247a4d01abee9966733d87f63c5f2a18d135b1b
Reza Heidari <m.heidari988@gmail.com>
2023-10-06 20:20:16 +0300

    Create date.js
    this is a simple function which converts a standard date string in to a shorter version eg. "Jul 2023"

---
+++ src/utils/date.js
@@ -0,0 +1,7 @@
+// Gets a date string and returns a shorter version eg. "Jun 2023"
+export function getMonthYear(dateString) {
+  const date = new Date(dateString);
+  const month = date.toLocaleString("default", { month: "short" });
+  const year = date.getFullYear();
+  return `${month} ${year}`;
+}
